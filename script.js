be0e7d1edaa1e10d71b1eb1b587c1c317dbf9504
anyoneout <chrisdafur@gmail.com>
2024-07-18 00:28:29 -0500

    added large goat images onclick fix

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+let imgNumber = document.images.length;
+console.log(imgNumber);
 
+function imgBorderCreator() {
+  for(let i=1; i < document.images.length; i++) {
+    let img;
+    img = document.images[i];
+    img.style.border = "10px solid black";
+  }
+
+}
+bordersButton.onclick = imgBorderCreator;
+
+
+  function largeGoatImg(clicked_id) {
+    var expandedGoat = document.getElementById(clicked_id);
+    
+    expandedGoat.style.width="100%";
+    expandedGoat.style.height="100%";
+    var url=expandedGoat.getAttribute('src');
+    window.open(url,'Image','width=expandedGoat.style.width, height=expandedGoat.style.height,resizable=1');
+      }
 
     /* let bordersClick = document.getElementById("bordersButton");
 
@@ -12,28 +33,4 @@
         .style.border = "thick solid black";
     }); */
  
-    let imgNumber = document.images.length;
-    console.log(imgNumber);
-
-    function imgBorderCreator() {
-      for(let i=0; i < document.images.length; i++) {
-        let img;
-        img = document.images[i];
-        img.style.border = "10px solid black";
-      }
-  
-    }
-    bordersButton.onclick = imgBorderCreator;
-
-
-    function largeGoatImg() {
-      var goatsArray = ["goat1","goat2","goat3","goat4","goat5","goat6","goat7","goat8","goat9","goat10","goat11","goat12",]
-      for (let i=0; i<goatsArray.length; i++) {
-      var largeImage = document.getElementById(goatsArray[i]);
-      largeImage.style.display = 'initial';
-      largeImage.style.width="100%";
-      largeImage.style.height="100%";
-      var url=largeImage.getAttribute('src');
-      window.open(url,'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1');
-      }
-   }
+   
