b227291af1707ca1ba17688a9eb99abed93b1940
anyoneout <chrisdafur@gmail.com>
2024-07-18 20:39:43 -0500

    test

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ let imgNumber = document.images.length;
 console.log(imgNumber);
 
 function imgBorderCreator() {
-  for(let i=1; i < document.images.length; i++) {
+  for (let i = 1; i < document.images.length; i++) {
     let img;
     img = document.images[i];
     img.style.border = "10px solid black";
-    }
+  }
 }
 bordersButton.onclick = imgBorderCreator;
 
 // open window with enlarged goat images
 function largeGoatImg(clicked_id) {
   let expandedGoat = document.getElementById(clicked_id);
-  expandedGoat.style.width="100%";
-  expandedGoat.style.height="100%";
-  let url=expandedGoat.getAttribute('src');
-  window.open(url,"Image","resizable=no,status=no, scrollbars=no, toolbar=no, menubar=no, width=expandedGoat.style.width, height=expandedGoat.style.height,");
+  expandedGoat.style.width = "100%";
+  expandedGoat.style.height = "100%";
+  let url = expandedGoat.getAttribute('src');
+  window.open(url, "Image", "resizable=no,status=no, scrollbars=no, toolbar=no, menubar=no, width=expandedGoat.style.width, height=expandedGoat.style.height,");
 }
 
 // make cheeses visible on button click
@@ -26,28 +26,29 @@ function cheeseChoice(clicked_id) {
 
   let cheeseOpen = document.getElementById(clicked_id);
 
-  if(cheeseOpen === freshButton) {
+  if (cheeseOpen === freshButton) {
     freshList.style.visibility = "visible";
   }
   else if (cheeseOpen === agedButton) {
-  agedList.style.visibility = "visible";
+    agedList.style.visibility = "visible";
   } else {
 
   }
 }
 
 
- // button event listener for borders     
-    /* let bordersClick = document.getElementById("bordersButton");
 
-    bordersClick.addEventListener("click", () => {
-      document.getElementById("farmImage")
-        .style.border = "thick solid black";
-    })
+// button event listener for borders     
+/* let bordersClick = document.getElementById("bordersButton");
+
+bordersClick.addEventListener("click", () => {
+  document.getElementById("farmImage")
+    .style.border = "thick solid black";
+})
  
-    bordersClick.addEventListener("click", () => {
-      document.getElementById("goatImages")
-        .style.border = "thick solid black";
-    }); */
- 
-   
+bordersClick.addEventListener("click", () => {
+  document.getElementById("goatImages")
+    .style.border = "thick solid black";
+}); */
+
+
